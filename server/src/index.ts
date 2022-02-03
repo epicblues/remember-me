a2147d59c709c5d4bc2ec667cc02ef85062f1ef2
Minsung Kim <epicblue@hanmail.net>
2022-02-03 21:42:19 +0900

    TypeORM DB 기초 설정 --local ver

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { User } from "./entities/User";
 
 const app = express();
 
-app.get("/", (_, res) => {
-  res.send("hello world");
-});
+(async () => {
+  await createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [User],
+    migrations: ["src/migration/**/*.ts"],
+    subscribers: ["src/subscriber/**/*.ts"],
+  });
 
-const port = process.env.PORT;
+  app.get("/", (_, res) => {
+    res.send("hello world");
+  });
+  //
+  const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`listening to port : ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`listening to port : ${port}`);
+  });
+})();
