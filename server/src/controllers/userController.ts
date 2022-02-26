1175e0c5db63b2f2f6636fe524087a2ed4e7ff56
Minsung Kim <epicblue@hanmail.net>
2022-02-26 14:31:22 +0900

    [Feat] 사용자 로그인, 로그아웃 컨트롤러 추가

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,8 +1,8 @@
 import { CustomRequestHandler } from "../types";
 import { User } from "../entities/User";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 
-export const registerUser: CustomRequestHandler = async (req, res) => {
+export const register: CustomRequestHandler = async (req, res) => {
   if (req.session.name) {
     res.send({ message: "이미 로그인한 상태입니다" });
     return;
@@ -17,4 +17,38 @@ export const registerUser: CustomRequestHandler = async (req, res) => {
     console.error(e);
     res.json({ message: "중복된 이름입니다." });
   }
+};
+
+export const login: CustomRequestHandler = async (req, res) => {
+  const { name, password } = req.body;
+  if (!name || !password) {
+    return res
+      .status(401)
+      .json({ message: "name 또는 password를 누락했습니다." });
+  }
+  const user = await User.findOne({ name });
+
+  if (!user) {
+    return res.status(401).json({ message: "존재하지 않는 이름입니다." });
+  }
+
+  const isPasswordCorrect = await compare(password, user.password);
+  if (!isPasswordCorrect) {
+    return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+  }
+
+  req.session.name = name;
+  return res.json({ messgae: `${name}님 환영합니다.` });
+};
+
+export const logout: CustomRequestHandler = (req, res) => {
+  const name = req.session.name;
+  if (!name) {
+    return res.status(401).json({ message: "로그인 된 상태가 아닙니다." });
+  }
+  req.session.destroy(() => {
+    return res.send({ message: `${name}님 로그아웃 되었습니다.` });
+  });
+
+  return;
 };
--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/userController";
+import { login, register, logout } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.post("/", registerUser);
+userRouter.post("/", register);
+userRouter.post("/login", login);
+userRouter.get("/logout", logout);
 
 export default userRouter;
